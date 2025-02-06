a909e311bec00cf143550d2a2e65bd92de035559
kinga.lindmayer <104622502+Lndy98@users.noreply.github.com>
2025-02-06 19:05:58 +0100

    refact Product creation

--- src/app/pages/new-product/new-product.component.ts
+++ src/app/pages/new-product/new-product.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms'
-import { Router } from '@angular/router';
-import { ProductInterface } from 'src/app/shared/models/ProductInterface';
-import { LocalStorageServiceService } from 'src/app/shared/services/local-storage-service.service';
-import { ProductService } from 'src/app/shared/services/products.service';
-import { v4 as uuidv4 } from 'uuid';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms'
+import {Router} from '@angular/router';
+import {ProductInterface} from 'src/app/shared/models/ProductInterface';
+import {LocalStorageServiceService} from 'src/app/shared/services/local-storage-service.service';
+import {ProductService} from 'src/app/shared/services/products.service';
+import {v4 as uuidv4} from 'uuid';
+import {ProductFactory} from "../../shared/factories/ProductFactory";
 
 @Component({
   selector: 'app-new-product',
@@ -28,72 +29,49 @@ export class NewProductComponent implements OnInit {
     incomingPrice: new FormControl()
   });
 
-  text : string = ""
+  text: string = ""
 
-  constructor(private router: Router,private productService: ProductService, private localStorage: LocalStorageServiceService) { }
+  constructor( private readonly productService: ProductService, private readonly localStorage: LocalStorageServiceService) {
+  }
 
   ngOnInit(): void {
   }
 
-  async save(){
+  async save() {
     this.text = "";
     let isSuccess = false;
     await this.createProduct();
-    if(this.text == ""){
+    //TODO:refact and test
+    if (this.text == "") {
+      console.log(this.product);
       this.productService.getByNumber(this.product.number).subscribe(async data => {
         console.log(data);
-        if(data.length == 0){
+        if (data.length == 0) {
+          console.log("isSuccess");
           isSuccess = true;
           await this.productService.create(this.product);
           this.localStorage.updateProduct(this.product);
           window.location.reload();
-        } else if (data.length != 0 && !isSuccess){
+        } else if (data.length != 0 && !isSuccess) {
           this.text = "A megadott cikkszám már létezik!";
         }
       })
     }
   }
 
-  createProduct(){
-    if(this.detailsForm.value.name && this.detailsForm.value.number && this.detailsForm.value.stock
-      && this.detailsForm.value.unit && this.detailsForm.value.price && this.detailsForm.value.incomingPrice){
-        this.product={
-          id: uuidv4(),
-          number: this.detailsForm.value.number.toUpperCase(),
-          name: this.detailsForm.value.name.toUpperCase(),
-          stock: this.detailsForm.value.stock,
-          unit: this.detailsForm.value.unit,
-          price: this.detailsForm.value.price,
-          incomingPrice: this.detailsForm.value.incomingPrice,
-          materialComposition: "",
-          grammWeight: "",
-          vtsz: "",
-          origin:""
-        }
-        if(this.detailsForm.value.materialComposition){
-          this.product.materialComposition = this.detailsForm.value.materialComposition;
-        }
-        if(this.detailsForm.value.grammWeight){
-          this.product.grammWeight = this.detailsForm.value.grammWeight;
-        }
-        if(this.detailsForm.value.vtsz){
-          this.product.vtsz = this.detailsForm.value.vtsz;
-        }
-        if(this.detailsForm.value.origin){
-          this.product.origin = this.detailsForm.value.origin;
-        }
-      } else {
-        this.text = "Minden csillaggal jelölt mezőt töltsön ki!"
-      }
-      return Promise.resolve('done');
+  createProduct() {
+    if (this.detailsForm.value.name && this.detailsForm.value.number && this.detailsForm.value.stock
+      && this.detailsForm.value.unit && this.detailsForm.value.price && this.detailsForm.value.incomingPrice) {
+      this.product = ProductFactory.createProduct(this.detailsForm.value.number, this.detailsForm.value.name, this.detailsForm.value.stock, this.detailsForm.value.unit,
+        this.detailsForm.value.price, this.detailsForm.value.incomingPrice, this.detailsForm.value.materialComposition, this.detailsForm.value.grammWeight,
+        this.detailsForm.value.vtsz, this.detailsForm.value.origin);
+    } else {
+      this.text = "Minden csillaggal jelölt mezőt töltsön ki!"
+    }
+    return Promise.resolve('done');
   }
 
-  cancel(){
+  cancel() {
     window.location.reload();
-  }
-  isProduct(productNumber: string):boolean{
-    let isProduct = false;
-
-    return isProduct;
   }
 }
--- src/app/shared/factories/ProductFactory.ts
+++ src/app/shared/factories/ProductFactory.ts
@@ -1,4 +1,6 @@
 import {ProductInterface} from "../models/ProductInterface";
+import {Product} from "../classes/Product";
+import {v4 as uuidv4} from "uuid";
 
 export class ProductFactory {
   static updateProductFromExcel(price: string, incomingPrice: string, stock: string, originProduct: ProductInterface): ProductInterface {
@@ -6,5 +8,21 @@ export class ProductFactory {
     originProduct.incomingPrice = incomingPrice ?? originProduct.incomingPrice;
     originProduct.stock = stock;
     return originProduct;
+  }
+
+  static createProduct(number: string, name: string, stock: string, unit: string, price: string, incomingPrice: string, materialComposition: any, grammWeight: string, vtsz: string, origin: string): ProductInterface {
+    return new Product(
+      uuidv4().toString(),
+      name.toUpperCase(),
+      number.toUpperCase(),
+      stock,
+      price,
+      unit,
+      incomingPrice,
+      materialComposition ?? "",
+      grammWeight ?? "",
+      vtsz ?? "",
+      origin ?? "",
+    );
   }
 }
